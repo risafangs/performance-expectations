4512be7f121b8b1f799ae73c8e12447e0cb4812f
Lisa Fang <lisa.fang@brooklyndata.co>
2023-11-19 22:27:35 -0600

    adjust insert

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ show_date = st.date_input("What's today's date?")
 show_name = st.selectbox("Pick one", ["DogProv", "Portal-prov", "Fire & Beer"])
 show_estimate = st.slider("How did you think the show would go?", 0, 10)
 show_actual = st.slider("How did the show actually go?", 0, 10)
+new_record = [show_date, show_name, show_estimate, show_actual]
 
 # Snowflake connection
 # doesn't work, st.connect or st.connection
 # conn = st.connect(**st.secrets["snowflake"])
 
 # initialize dataframe
-df = pd.DataFrame(show_date, show_name, show_estimate, show_actual, columns=["date", "name", "estimate", "actual"])
+df = pd.DataFrame(new_record, columns=["raw_input"])
 
 st.dataframe(df)
