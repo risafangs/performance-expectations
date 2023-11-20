db7a83a6ee39dea8a7923af43a343215d407c9ac
Lisa Fang <lisa.fang@brooklyndata.co>
2023-11-20 12:05:51 -0600

    try gsheets connection

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
 import streamlit as st
 import pandas as pd
-from snowflake.snowpark import Session
+#from snowflake.snowpark import Session
+from streamlit_gsheets import GSheetsConnection
 
 st.header("Mark's Very Good and Official Performance Information")
 
 # inputs
-show_date = st.date_input("What's today's date?")
+date = st.date_input("What's today's date?")
 show_name = st.selectbox("Pick one", ["DogProv", "Portal-prov", "Fire & Beer"])
 score_estimate = st.slider("How did you think the show would go?", 0, 10)
 score_actual = st.slider("How did the show actually go?", 0, 10)
 # new_input = dict(show_date, show_name, score_estimate, score_actual)
 
-# Snowflake connection
-# connect doesn't work
-# error is about name param
-# conn = st.connection(**st.secrets["snowflake"], type='sql')
-
-# conn = st.connection(**st.secrets["snowflake_credentials"])
-
 # Establish Snowflake session
-# this kind of works? but cannot open the SSO browser (connection error)
-@st.cache_resource
-def create_session():
-    return Session.builder.configs(st.secrets.snowflake).create()
+# this doesn't work - the SSO redirect fails
+#@st.cache_resource
+#def create_session():
+#    return Session.builder.configs(st.secrets.snowflake).create()
 
-session = create_session()
-st.success("Connected to Snowflake!")
+#session = create_session()
+#st.success("Connected to Snowflake!")
 
-# initialize dataframe
-# not working
-# df = pd.DataFrame(new_input)
+# Connect to google sheets
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-st.dataframe(df)
+df = conn.read()
+
+# Print Sheet results
+#for row in df.itertuples():
+#    st.write(f"The {row.show_name} show on {row.date} had an estimated score of {row.score_estimate} but was actually a {row.score_actual}") 
