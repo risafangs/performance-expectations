c3e455f53cb9c5f2ba69c76e9e0a10ed5bc59e61
lisa fang <risafangs@users.noreply.github.com>
2023-11-19 21:04:59 -0600

    add Snowflake connection

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import streamlit as st
 import pandas as pd
+import snowflake.connector
 
 st.header("Mark's Very Good and Official Performance Information")
 
+# inputs
 show_date = st.date_input("What's today's date?")
 show_name = st.selectbox("Pick one", ["DogProv", "Portal-prov", "Fire & Beer"])
 show_estimate = st.slider("How did you think the show would go?", 0, 10)
 show_actual = st.slider("How did the show actually go?", 0, 10)
 
+# Snowflake connection
+connection = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
-# st.dataframe()
+# add inputs to Snowflake
+new_inputs = insert_row_snowflake(show_date, show_name, show_estimate, show_actual)
+streamlit.text(new_inputs)
