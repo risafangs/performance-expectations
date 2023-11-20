fcb5e7c04ac44758bbcd4d99452e73ffbaf928d1
Lisa Fang <lisa.fang@brooklyndata.co>
2023-11-19 22:34:32 -0600

    try pivoting column

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ st.header("Mark's Very Good and Official Performance Information")
 # inputs
 show_date = st.date_input("What's today's date?")
 show_name = st.selectbox("Pick one", ["DogProv", "Portal-prov", "Fire & Beer"])
-show_estimate = st.slider("How did you think the show would go?", 0, 10)
-show_actual = st.slider("How did the show actually go?", 0, 10)
-new_record = [show_date, show_name, show_estimate, show_actual]
+score_estimate = st.slider("How did you think the show would go?", 0, 10)
+score_actual = st.slider("How did the show actually go?", 0, 10)
+new_input = dict(show_date, show_name, score_estimate, score_actual)
 
 # Snowflake connection
 # doesn't work, st.connect or st.connection
 # conn = st.connect(**st.secrets["snowflake"])
 
 # initialize dataframe
-df = pd.DataFrame(new_record, columns=["raw_input"])
+df = pd.DataFrame(new_input)
 
 st.dataframe(df)
