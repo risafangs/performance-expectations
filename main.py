6d492c0d79ccb87da72c3661ae1eb575ea9c5090
Lisa Fang <lisa.fang@brooklyndata.co>
2023-11-20 12:52:42 -0600

    remove snowpark code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-#from snowflake.snowpark import Session
 from streamlit_gsheets import GSheetsConnection
 
 st.header("Mark's Very Good and Official Performance Information")
@@ -10,16 +9,6 @@ date = st.date_input("What's today's date?")
 show_name = st.selectbox("Pick one", ["DogProv", "Portal-prov", "Fire & Beer"])
 score_estimate = st.slider("How did you think the show would go?", 0, 10)
 score_actual = st.slider("How did the show actually go?", 0, 10)
-# new_input = dict(show_date, show_name, score_estimate, score_actual)
-
-# Establish Snowflake session
-# this doesn't work - the SSO redirect fails
-#@st.cache_resource
-#def create_session():
-#    return Session.builder.configs(st.secrets.snowflake).create()
-
-#session = create_session()
-#st.success("Connected to Snowflake!")
 
 # Connect to google sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
