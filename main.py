66d96b5560c803005673240e2c4dea59a48636fb
Lisa Fang <lisa.fang@brooklyndata.co>
2023-11-19 22:47:40 -0600

    comment out other error

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ show_date = st.date_input("What's today's date?")
 show_name = st.selectbox("Pick one", ["DogProv", "Portal-prov", "Fire & Beer"])
 score_estimate = st.slider("How did you think the show would go?", 0, 10)
 score_actual = st.slider("How did the show actually go?", 0, 10)
-new_input = dict(show_date, show_name, score_estimate, score_actual)
+# new_input = dict(show_date, show_name, score_estimate, score_actual)
 
 # Snowflake connection
 # doesn't work, st.connect or st.connection
-# conn = st.connect(**st.secrets["snowflake"])
+conn = st.connect(**st.secrets["snowflake"])
 
 # initialize dataframe
 # not working
